bb89d355a5d15bcb7733cb3e87812ab5d48a39eb
Skylar Kwon <ddauppo@gmail.com>
2021-03-26 15:41:07 -0400

    Fixs issue#26 - add gender options

--- src/screens/GetUserInfoScreen.js
+++ src/screens/GetUserInfoScreen.js
@@ -43,6 +43,7 @@ const GetUserInfoScreen = ({ navigation }) => {
     { label: "Male", value: "Male" },
     { label: "Female", value: "Female" },
     { label: "Other", value: "Other" },
+    { label: "Prefer not to answer", value: "Prefer not to answer" },
   ];
   return (
     <View style={screen.container}>
