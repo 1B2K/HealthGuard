f29cb45913d5375ec83159ddd4b76901deab343f
anthony <anthonylomagno@gmail.com>
2021-05-13 20:47:06 -0400

    added suggested changes

--- src/screens/TermsConditionScreen.js
+++ src/screens/TermsConditionScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Button, Text, SafeAreaView, View } from "react-native";
+import { StyleSheet, Button, Text, SafeAreaView, View, ScrollView } from "react-native";
 
 import { TouchableOpacity } from "react-native-gesture-handler";
 
@@ -15,41 +15,46 @@ const TermsConditionScreen = ({ navigation }) => {
     <SafeAreaView style={screenConfig.container}>
       <View style={styles.screenContainer}>
 
-        <Text style={screenConfig.subTitleText}>
-          Welcome to
-        </Text>
-        <Text style={screenConfig.titleText}>
-          Health Guard!
-        </Text>
-
-        <View style={styles.cardContainer}>
-          <Text style={styles.cardTitle}>Terms & Conditions</Text>
-          <Text style={styles.cardText}>
-
-            Thank you for using Health Guard! {"\n\n"}
-            We're happy you're here. Please read this Terms of Service agreement
-            carefully before accessing or using Health Guard. Because it is such an
-            important contract between us and our users, we have tried to make it as
-            clear as possible.
-            {"\n"}
-            This App provides information only and does not operate to as a
-            diagnosis tool and it does not subtitute for professional medical advice
-            and assistance.
-            {"\n"}
-            You will be provided vaccination recommendation and also you can manage
-            your vaccination history through this App. your information such as
-            gender and birth date will not be used anywhere except in this app for
-            recommending vaccination.
-            {"\n"}
-            By clicking below, you acknowledge and agree to our use of your personal
-            information we collect through the use of this app in the ways described
-            in our privacy notice.
-            
-            {"\n"}
+        <View style={styles.titleContainer}>
+          <Text style={screenConfig.subTitleText}>
+            Welcome to
+          </Text>
+          <Text style={screenConfig.titleText}>
+            Health Guard!
           </Text>
         </View>
+       
+        <View style={styles.cardContainer}>
+          <Text style={styles.cardTitle}>Terms & Conditions</Text>
+          <ScrollView >
+            <Text style={styles.cardText}>
 
-        <View style={styles.saveButtonContainer}>
+              Thank you for using Health Guard! {"\n\n"}
+              We're happy you're here. Please read this Terms of Service agreement
+              carefully before accessing or using Health Guard. Because it is such an
+              important contract between us and our users, we have tried to make it as
+              clear as possible.
+              {"\n"}
+              This App provides information only and does not operate to as a
+              diagnosis tool and it does not subtitute for professional medical advice
+              and assistance.
+              {"\n"}
+              You will be provided vaccination recommendation and also you can manage
+              your vaccination history through this App. your information such as
+              gender and birth date will not be used anywhere except in this app for
+              recommending vaccination.
+              {"\n"}
+              By clicking below, you acknowledge and agree to our use of your personal
+              information we collect through the use of this app in the ways described
+              in our privacy notice.
+              
+              {"\n"}
+            </Text>
+          </ScrollView>
+        </View>
+
+
+        <View style={styles.buttonContainer}>
           <TouchableOpacity
             style={screenConfig.buttonContainer}
             onPress={() => navigation.navigate("ChildrenScreen")}
@@ -65,16 +70,24 @@ const TermsConditionScreen = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   screenContainer: {
+    flex: 1,
     marginTop: verticalScale(40),
     paddingLeft: moderateScale(24),
     paddingRight: moderateScale(24),
+    backgroundColor: colors.back
+  },
+  titleContainer: {
   },
   cardContainer: {
+    flex: 1,
     elevation: 1,
     padding: moderateScale(24),
     marginTop: verticalScale(16),
     marginBottom: verticalScale(16),
     borderRadius: moderateScale(16)
+  },
+  buttonContainer: {
+    flex: 1
   },
   cardTitle: {
     textAlign: "center",
