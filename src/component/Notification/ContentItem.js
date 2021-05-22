4bae181345027242d49ef6e4d7d157a61680af10
Elisa <elisangli11@gmail.com>
2021-05-21 22:47:44 -0400

    Fix #80 update component and notification view

--- src/component/Notification/ContentItem.js
+++ src/component/Notification/ContentItem.js
@@ -5,21 +5,13 @@ import colorConfig from "../../config/colors";
 
 class ContentItem extends Component {
   render = () => {
-    const { key, status, name, vaccineName, date } = this.props;
+    const { key, name, vaccineName, date } = this.props;
     return (
       <View key={key}>
-        <View style={styles.textContainer}>
-          {status === "TODAY" ? (
-            <Text style={styles.status}>{status}</Text>
-          ) : (
-            <Text style={styles.status2}>{status}</Text>
-          )}
-
-          <View style={styles.contentContainer}>
-            <Text style={styles.name}>{name}</Text>
-            <Text style={styles.type}> {vaccineName}</Text>
-            <Text style={styles.date}> {date}</Text>
-          </View>
+        <View style={styles.contentContainer}>
+          <Text style={styles.name}>{name}</Text>
+          <Text style={styles.type}> {vaccineName}</Text>
+          <Text style={styles.date}> {date}</Text>
         </View>
       </View>
     );
@@ -35,30 +27,23 @@ const styles = StyleSheet.create({
     fontFamily: "notoSans-regular",
     fontSize: moderateScale(12),
     marginLeft: scale(15),
+    textAlign: "right",
   },
   textContainer: {
     marginLeft: scale(15),
     marginTop: verticalScale(15),
   },
-  status: {
-    fontFamily: "fredokaOne-regular",
-    fontSize: moderateScale(12),
-    color: colorConfig.red,
-  },
-  status2: {
-    fontFamily: "fredokaOne-regular",
-    fontSize: moderateScale(12),
-    color: colorConfig.blue,
-  },
   type: {
     fontFamily: "notoSans-regular",
     fontSize: moderateScale(12),
     marginLeft: scale(25),
+    textAlign: "center",
   },
   name: {
     fontFamily: "notoSans-regular",
     fontSize: moderateScale(12),
     fontWeight: "bold",
+    textAlign: "left",
   },
 });
 
--- src/screens/NotificationScreen.js
+++ src/screens/NotificationScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, View, FlatList } from "react-native";
+import { SafeAreaView, StyleSheet, View, Text } from "react-native";
 
 import { scale, moderateScale, verticalScale } from "../config/scaling";
 import colorConfig from "../config/colors";
@@ -11,52 +11,106 @@ const NotificationScreen = ({ navigation }) => {
   const notification = [
     {
       status: "TODAY",
-      name: "Jasper Matthews",
+      name: "Jasper Matthews1",
       vaccineName: "COVID-19 Vaccine",
       date: "April 15th",
-      id: "s",
     },
     {
       status: "TODAY",
-      name: "Jasper Matthews",
-      vaccineName: "COVID-19 Vaccine",
+      name: "Jasper Matthews2",
+      vaccineName: "COVID-19 Vaccine2",
       date: "April 15th",
-      id: "2",
     },
     {
       status: "UPCOMING",
-      name: "Jasper Matthews",
-      vaccineName: "COVID-19 Vaccine",
+      name: "Jasper Matthews3",
+      vaccineName: "COVID-19 Vaccine3",
       date: "April 15th",
-      id: "3",
     },
     {
       status: "OVERDUE",
-      name: "Jasper Matthews",
-      vaccineName: "COVID-19 Vaccine",
+      name: "Jasper Matthews4",
+      vaccineName: "COVID-19 Vaccine4",
       date: "April 15th",
-      id: "4",
     },
   ];
 
-  const renderItem = ({ item, index }) => (
-    <ContentItem
-      key={index}
-      status={item.status}
-      name={item.name}
-      vaccineName={item.vaccineName}
-      date={item.date}
-    />
-  );
+  const statusUpcoming = () => {
+    return (
+      <View>
+        {notification
+          .filter((item) => item.status === "UPCOMING")
+          .map((item, index) => {
+            return (
+              <ContentItem
+                key={index}
+                name={item.name}
+                vaccineName={item.vaccineName}
+                date={item.date}
+              />
+            );
+          })}
+      </View>
+    );
+  };
+
+  const statusOverdue = () => {
+    return (
+      <View>
+        {notification
+          .filter((item) => item.status === "OVERDUE")
+          .map((item, index) => {
+            return (
+              <ContentItem
+                key={index}
+                name={item.name}
+                vaccineName={item.vaccineName}
+                date={item.date}
+              />
+            );
+          })}
+      </View>
+    );
+  };
+
+  const statusToday = () => {
+    return (
+      <View>
+        {notification
+          .filter((item) => item.status === "TODAY")
+          .map((item, index) => {
+            return (
+              <ContentItem
+                key={index}
+                name={item.name}
+                vaccineName={item.vaccineName}
+                date={item.date}
+              />
+            );
+          })}
+      </View>
+    );
+  };
 
   return (
     <SafeAreaView style={screenConfig.container}>
-      <View style={styles.container}>
-        <FlatList
-          data={notification}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id}
-        />
+      <View style={styles.textContainer}>
+        <View style={styles.container}>
+          <Text style={[styles.status, styles.red]}>{"TODAY"}</Text>
+          {statusToday()}
+        </View>
+      </View>
+      <View style={styles.textContainer}>
+        <View style={styles.container}>
+          <Text style={[styles.status, styles.blue]}>{"UPCOMING"}</Text>
+          {statusUpcoming()}
+        </View>
+      </View>
+      <View style={styles.textContainer}>
+        <View style={styles.container}>
+          <Text style={[styles.status, styles.blue]}>{"OVERDUE"}</Text>
+          {statusOverdue()}
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -64,9 +118,24 @@ const NotificationScreen = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   container: {
+    marginLeft: scale(15),
+    marginTop: verticalScale(15),
+    marginBottom: verticalScale(15),
+  },
+  textContainer: {
     backgroundColor: colorConfig.white,
     width: scale(349),
     marginTop: verticalScale(10),
+  },
+  status: {
+    fontFamily: "fredokaOne-regular",
+    fontSize: moderateScale(12),
+  },
+  red: {
+    color: colorConfig.red,
+  },
+  blue: {
+    color: colorConfig.blue,
   },
 });
 
